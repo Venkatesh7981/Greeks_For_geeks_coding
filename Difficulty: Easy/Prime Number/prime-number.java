e19d7fc78618e3bb18d3fd8f1aeb4dc1b26cc826
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2026-05-31 19:21:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        int count=0;
+        if(n<=1) return false;
+      for(int i=1;i<=n;i++ ){
+          if(n%i==0){
+              count++;
+          }
+      }
+          if(count==2)
+          return true;
+          else return false;
+       
+    }
+}
