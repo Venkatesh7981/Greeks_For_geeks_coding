1b890ce29ab087b274cc1c33f919405b335fccf5
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2026-06-13 18:26:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        // code here
+       LinkedHashSet<Integer> set=new LinkedHashSet<>();
+         for(int num:arr){
+             set.add(num);
+         }
+         return new ArrayList<>(set);
+    }
+}
