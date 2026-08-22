13afbdc42d875fef43d20187dff3493b023c13a1
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2026-08-22 20:25:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Matching Pair/matching-pair.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int find(int n) {
+        // code here
+        int count=1;
+        for(int i=1;i<=n;i++){
+            count++;
+        }
+        return count;
+    }
+}
