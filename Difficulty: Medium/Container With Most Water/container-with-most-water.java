d8982801bddc1b38c920cddf895847d80c3c3c33
Kuruva Venkatesh <159419272+Venkatesh7981@users.noreply.github.com>
2026-09-17 18:51:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxWater(int nums[]) {
+        // Code Here
+        int i=0;
+                int j=nums.length-1;
+                int max=0;
+                while(i<j){
+                    int wid=j-i;
+                    int hig=Math.min(nums[i],nums[j]);
+                    int area=wid*hig;
+                    max=Math.max(max,area);
+                    if(nums[i]<nums[j]){
+                        i++;
+                    }
+                    else {
+                        j--;
+                    }
+                }
+                return max;
+    }
+}
